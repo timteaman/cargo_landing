4d55d1594087cfeabc02a2989927b0c9d7378852
timteaman <tim.like.tea@gmail.com>
2024-09-26 18:10:11 +0200

    fixed bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,33 @@
 // fixed header
-
-const headerEl = document.querySelector('.header');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > headerEl.offsetHeight) {
-    headerEl.classList.add('scrolled');
-  } else {
-    headerEl.classList.remove('scrolled');
-  }
-});
-
-// burger
-
-const burgerBtnEl = document.querySelector('.burger');
-
-const handleBurgerClick = () => {
-  burgerBtnEl.classList.toggle('burger--active');
-};
-
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('burger')) {
-    handleBurgerClick();
-  }
-});
-
-//animation
-
 document.addEventListener('DOMContentLoaded', () => {
-  const section = document.querySelector('.how-work');
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        section.classList.add('how-work--active');
-        section.classList.remove('how-work--hide');
-      } else {
-        section.classList.remove('how-work--active');
-        section.classList.add('how-work--hide');
-      }
-    });
+  const headerEl = document.querySelector('.header');
+  window.addEventListener('scroll', () => {
+    headerEl.classList.toggle(
+      'scrolled',
+      window.scrollY > headerEl.offsetHeight
+    );
   });
 
+  // burger
+  const burgerBtnEl = document.querySelector('.burger');
+  burgerBtnEl.addEventListener('click', () => {
+    burgerBtnEl.classList.toggle('burger--active');
+  });
+
+  // close mobile menu if click
+  const menuEl = document.querySelector('.menu');
+  menuEl.addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu__link')) {
+      burgerBtnEl.classList.remove('burger--active');
+      menuEl.classList.remove('menu--active');
+    }
+  });
+
+  // animation section how-work
+  const section = document.querySelector('.how-work');
+  const observer = new IntersectionObserver(([entry]) => {
+    section.classList.toggle('how-work--active', entry.isIntersecting);
+    section.classList.toggle('how-work--hide', !entry.isIntersecting);
+  });
   observer.observe(section);
 });
